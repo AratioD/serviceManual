4d4f5cfc47eb7fcd5ee0ff5183e17514a85b1205
AratioD <fffb@ddd.com>
2020-04-02 22:04:47 +0300

    created many new html components

--- angular8-crud/src/app/app.module.ts
+++ angular8-crud/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductsAddComponent } from './products-add/products-add.component';
+import { ProductsEditComponent } from './products-edit/products-edit.component';
+import { ProductsDetailComponent } from './products-detail/products-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductsAddComponent,
+    ProductsEditComponent,
+    ProductsDetailComponent
   ],
   imports: [
     BrowserModule,
