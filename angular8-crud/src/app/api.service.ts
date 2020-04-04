36bdde355b5a927a06028a8b219a9cf763d3c3f8
AratioD <fffb@ddd.com>
2020-04-04 12:17:41 +0300

    constructor added

--- angular8-crud/src/app/api.service.ts
+++ angular8-crud/src/app/api.service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@angular/core';
-import { Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { catchError, tap, map } from 'rxjs/operators';
-import { Product } from './product';
+import { Injectable } from "@angular/core";
+import { Observable, of, throwError } from "rxjs";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { catchError, tap, map } from "rxjs/operators";
+import { Product } from "./product";
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
+  headers: new HttpHeaders({ "Content-Type": "application/json" }),
 };
 const apiUrl = "/api/v1/products";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
