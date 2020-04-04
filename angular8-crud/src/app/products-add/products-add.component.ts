cf2db2fcc32d061080ee67d5b788d6508d7c2251
AratioD <fffb@ddd.com>
2020-04-04 15:20:18 +0300

    lot of shitty changes

--- angular8-crud/src/app/products-add/products-add.component.ts
+++ angular8-crud/src/app/products-add/products-add.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { ApiService } from "../api.service";
+import {
+  FormControl,
+  FormGroupDirective,
+  FormBuilder,
+  FormGroup,
+  NgForm,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-products-add',
-  templateUrl: './products-add.component.html',
-  styleUrls: ['./products-add.component.scss']
+  selector: "app-products-add",
+  templateUrl: "./products-add.component.html",
+  styleUrls: ["./products-add.component.scss"],
 })
 export class ProductsAddComponent implements OnInit {
+  productForm: FormGroup;
+  prod_name = "";
+  prod_desc = "";
+  prod_price: number = null;
+  isLoadingResults = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
